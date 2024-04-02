5ad61651e371d9e7897cbcc3ffa3693eea660365
Cannicakee <stanfill11@gmail.com>
2024-04-02 17:43:02 -0600

    Solved Problem 10

--- aa02-try-out-callbacks/problems/10-my-every.js
+++ aa02-try-out-callbacks/problems/10-my-every.js
@@ -22,9 +22,27 @@ console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 *******************************************************************************/
 
 function myEvery(array, cb) {
-    // Your code here 
+    for(let i = 0; i < array.length; i++){
+        let el = array[i];
+        if(cb(el) === false) {
+            return false;
+        }
+    }
+    return true;
 }
 
+let isEven = function (num) {
+    return num % 2 === 0;
+};
+
+let hasO = function(string) {
+    return string.includes('o');
+};
+
+console.log(myEvery([4, 8, 6, 10], isEven));            // true
+console.log(myEvery([4, 7, 6, 10], isEven));            // false
+console.log(myEvery(['book', 'door'], hasO));           // true
+console.log(myEvery(['book', 'door', 'pen'], hasO));    // false
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myEvery;
