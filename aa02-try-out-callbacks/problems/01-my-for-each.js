6f777ca93de9ade624bfa82e771528f7d1602550
Cannicakee <stanfill11@gmail.com>
2024-04-02 15:47:41 -0600

    install commit

--- aa02-try-out-callbacks/problems/01-my-for-each.js
+++ aa02-try-out-callbacks/problems/01-my-for-each.js
@@ -22,8 +22,17 @@ console.log(test); // ['LAIKA', 'BELKA']
 *******************************************************************************/
 
 function myForEach(array, cb) {
-    // Your code here 
+    for(let i = 0; i < array.length; i++){
+        let el = array[i]
+        cb(el, i, array)
+    }
 }
+
+let test = [];
+myForEach(['laika', 'belka'], function (el) {
+    test.push(el.toUpperCase());
+});
+console.log(test);
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
