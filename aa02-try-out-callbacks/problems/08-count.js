cbe0941119041f179df67f120ded2264a8edb525
Cannicakee <stanfill11@gmail.com>
2024-04-02 17:16:39 -0600

    Solved Problem 8

--- aa02-try-out-callbacks/problems/08-count.js
+++ aa02-try-out-callbacks/problems/08-count.js
@@ -27,9 +27,35 @@ console.log(result4); // 0
 *******************************************************************************/
 
 function count(array, cb) {
-  // Your code here 
+  let counter = 0;
+  for(let i = 0; i < array.length; i++) {
+    let el = array[i];
+    if(cb(el) === true) {
+      counter++;
+    }
+  }
+  return counter;
 }
 
+let result1 = count([18, 5, 32, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result1); // 3
+
+let result2 = count([17, 5, 31, 7, 100], function (n) {
+  return n % 2 === 0;
+});
+console.log(result2); // 1
+
+let result3 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('o');
+});
+console.log(result3); // 3
+
+let result4 = count(['follow', 'the', 'yellow', 'brick', 'road'], function (str) {
+  return str.includes('x');
+});
+console.log(result4); // 0
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = count;
