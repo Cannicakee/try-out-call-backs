908c907fa0eb9da449b190447655d8bafd832ff0
Cannicakee <stanfill11@gmail.com>
2024-04-02 17:03:28 -0600

    Solved Problem 6

--- aa02-try-out-callbacks/problems/06-reject.js
+++ aa02-try-out-callbacks/problems/06-reject.js
@@ -22,9 +22,26 @@ console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth'
 *******************************************************************************/
 
 function reject(array, cb) {
-  // Your code here 
+    let newArray = [];
+
+    for(let i = 0; i < array.length; i++) {
+      let el = array[i];
+      if(cb(el, i, array) === false) {
+        newArray.push(el);
+      }
+    }
+    return newArray;
 }
 
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+console.log(reject([7, 8, 5, 6, 12, 11], isEven)); // [ 7, 5, 11 ]
+
+let hasA = function(s) {
+  return s.toLowerCase().includes('a');
+};
+console.log(reject(['breadth', 'GRAPH', 'depth', 'height'], hasA)); // [ 'depth', 'height' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = reject;
