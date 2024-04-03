053c6e8cb2a09f5ab355142728b9c7e5f38636e2
Cannicakee <stanfill11@gmail.com>
2024-04-02 22:53:51 -0600

    Solved Problem 17

--- aa02-try-out-callbacks/problems/17-xor-select.js
+++ aa02-try-out-callbacks/problems/17-xor-select.js
@@ -32,9 +32,39 @@ console.log(
 *******************************************************************************/
 
 let xorSelect = function(array, cb1, cb2) {
-  // Your code here 
+  let newArray = [];
+  array.forEach((boo) => {
+    if((cb1(boo) === true && cb2(boo) !== true) || (cb1(boo) !== true && cb2(boo) == true)) {
+      newArray.push(boo);
+    } 
+  })
+  return newArray;
 };
 
+let isEven = function(n) {
+  return n % 2 === 0;
+};
+
+let isPositive = function(n) {
+  return n > 0;
+};
+
+console.log(xorSelect([-2, -1, 1, 2, 3, 4], isEven, isPositive));
+// [ -2, 1, 3 ]
+
+
+let longString = function(s) {
+  return s.length > 4;
+};
+
+let startsA = function(s) {
+  return s[0] === "a";
+};
+
+console.log(
+  xorSelect(["art", "academy", "app", "cat", "buttery"], longString, startsA)
+);
+// [ 'art', 'app', 'buttery' ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = xorSelect;
