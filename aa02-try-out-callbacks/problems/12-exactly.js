e28b638a0074148cabeb14ec723a5cdde2910462
Cannicakee <stanfill11@gmail.com>
2024-04-02 18:10:28 -0600

    Solved Problem 12

--- aa02-try-out-callbacks/problems/12-exactly.js
+++ aa02-try-out-callbacks/problems/12-exactly.js
@@ -28,9 +28,29 @@ console.log(result4); // true
 *******************************************************************************/
 
 function exactly(array, num, cb) {
-  // Your code here 
+ let count = array.filter(cb).length;
+  return count === num;
 }
 
+let result1 = exactly([18, 5, 32, 7, 100], 3, function (n) {
+  return n % 2 === 0;
+});
+console.log(result1); // true
+
+let result2 = exactly([18, 5, 32, 7, 100], 2, function (n) {
+  return n % 2 === 0;
+});
+console.log(result2); // false
+
+let result3 = exactly(['follow', 'the', 'yellow', 'brick', 'road'], 1, function (str) {
+  return str.includes('x');
+});
+console.log(result3); // false
+
+let result4 = exactly(['follow', 'the', 'yellow', 'brick', 'road'], 0, function (str) {
+  return str.includes('x');
+});
+console.log(result4); // true
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = exactly;
