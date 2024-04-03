261f88b788f3154c6f9481a7b2a85a3556530144
Cannicakee <stanfill11@gmail.com>
2024-04-02 21:24:05 -0600

    Solved Problem 15

--- aa02-try-out-callbacks/problems/15-sentence-mapper.js
+++ aa02-try-out-callbacks/problems/15-sentence-mapper.js
@@ -26,9 +26,30 @@ console.log(result2); // 'ths s prtty cl rght'
 *******************************************************************************/
 
 let sentenceMapper = function (sentence, cb) {
-  // Your code here 
+  let words = sentence.split(' ');
+  let wordMap = words.map(cb);
+  return wordMap.join(' ');
 };
 
+
+let result1 = sentenceMapper("what is the answer?", function(word) {
+  return word.toUpperCase() + "!";
+});
+console.log(result1); // 'WHAT! IS! THE! ANSWER?!'
+
+let removeVowels = function(word) {
+  let newWord = "";
+  for (let i = 0; i < word.length; i++) {
+      let char = word[i];
+      if (!"aeiou".includes(char)) {
+          newWord += char;
+      }
+  }
+  return newWord;
+};
+
+let result2 = sentenceMapper("this is pretty cool right", removeVowels);
+console.log(result2); // 'ths s prtty cl rght'
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = sentenceMapper;
