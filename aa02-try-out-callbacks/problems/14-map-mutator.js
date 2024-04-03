55368c600247edc47764f0feaa9f8286ebd91764
Cannicakee <stanfill11@gmail.com>
2024-04-02 21:10:03 -0600

    Solved Problem 14

--- aa02-try-out-callbacks/problems/14-map-mutator.js
+++ aa02-try-out-callbacks/problems/14-map-mutator.js
@@ -18,10 +18,34 @@ mapMutator(arr2, function (el, i) {
 console.log(arr2); // [ 0, 9, 20 ]
 *******************************************************************************/
 
+// function mapMutator(array, cb) {
+//   for(let i = 0; i < array.length; i++) {
+//     let el = array[i];
+//     let newNum = cb(el);
+//     array.splice(i, 1, newNum);
+//   }
+// }
 function mapMutator(array, cb) {
-  // Your code here 
+  let mutator = array.forEach((num, index) => {
+    let newNum = cb(num, index);
+    array.splice(index, 1, newNum);
+  })
+  return mutator;
 }
 
+
+
+let arr1 = [4, 2, 6, 5];
+mapMutator(arr1, function (el) {
+    return el * 2;
+});
+console.log(arr1);  // [ 8, 4, 12, 10 ]
+
+let arr2 = [8, 9, 10];
+mapMutator(arr2, function (el, i) {
+    return el * i;
+});
+console.log(arr2); // [ 0, 9, 20 ]
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
   module.exports = mapMutator;
